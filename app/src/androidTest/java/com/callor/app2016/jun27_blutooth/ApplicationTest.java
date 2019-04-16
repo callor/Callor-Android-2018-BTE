894fa3cffb86f116549dfb28035cae199fd9d61f
callor <callor@callor.com>
2019-04-16 19:26:36 +0900

    2019-04-16

---
+++ app/src/androidTest/java/com/callor/app2016/jun27_blutooth/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.callor.app2016.jun27_blutooth;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
